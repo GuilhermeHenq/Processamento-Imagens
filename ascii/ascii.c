fd7ae04e46d8fa91a06c08960eeabf9f91abc6c1
Guilherme Henrique Pereira Serafini <108468458+GuilhermeHenq@users.noreply.github.com>
2023-04-28 15:05:18 -0300

    Add files via upload

---
+++ ascii/ascii.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "imagelib.h"
+
+
+void ascii(image In, char* nameOut, int numCols, int numRows, char* symbols, int numSymbols) {
+   int i, j, k, idx;
+   double teste;
+   FILE* fpOut = fopen(nameOut, "w");
+
+
+   if (!fpOut) {
+       printf("Erro ao abrir o arquivo de saída.\n");
+       exit(1);
+   }
+
+
+   double range = 255.0 / (numSymbols - 1);
+
+
+   for (i = 0; i < numRows; i++) {
+       for (j = 0; j < numCols; j++) {
+           teste = In[i*numCols + j];
+
+
+           idx = (int) (teste / range);
+           if (idx >= numSymbols) idx = numSymbols - 1;
+
+
+           for (k = 0; k < strlen(symbols); k++) {
+               if (k == idx) {
+                   fputc(symbols[k], fpOut);
+                   break;
+               }
+           }
+       }
+       fputc('\n', fpOut);
+   }
+   fclose(fpOut);
+}
+
+
+int main(int argc, char *argv[]) {
+   image In;
+   int nr, nc, ml;
+   char *nameIn = argv[1];
+   char *nameOut = "Saida.txt";
+   int numCols = atoi(argv[2]);
+   int numRows = atoi(argv[3]);
+   char *symbols = argv[4];
+   int numSymbols = strlen(symbols);
+
+
+   In = img_get(nameIn, &nr, &nc, &ml, GRAY);
+//   In = img_resample(In, nr, nc, numRows, numCols);
+   ascii(In, nameOut, numCols, numRows, symbols, numSymbols);
+   img_free(In);
+
+
+   return 0;
+}
+
+
